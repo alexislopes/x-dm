e466c60f7852dbae240b53068be1f5f8123e5e1e
panzee <panzee@DESKTOP-U1S2FQS>
2023-03-15 00:25:29 -0300

    add twitter dm component

---
+++ src/components/TwitterDM.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container__dm" :class="{ 'opened': opened }">
+    <div class="header__dm__container">
+      <div class="header__dm" @click="opened = !opened">
+        <p>Messages</p>
+        <div class="icons__dm">
+          <IconNewMessage class="icon__dm" />
+          <IconExpand class="icon__dm" :class="{ 'opened': !opened }" />
+        </div>
+      </div>
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<script setup>
+import IconExpand from "../components/icons/IconExpand.vue";
+import IconNewMessage from "./icons/IconNewMessage.vue";
+
+import { ref } from "vue";
+const opened = ref(false)
+</script>
+
+<style>
+.container__dm {
+  @apply w-[400px] absolute bottom-0 right-0 mr-4 content-center bg-[#15202b] rounded-t-2xl
+}
+
+.container__dm.opened {
+  @apply bottom-2/4
+}
+
+.icon__dm {
+  @apply w-[19px] h-[19px] cursor-pointer fill-[#f7f9f9];
+  transition: transform .5s ease;
+}
+
+.icon__dm.opened {
+  transform: rotate(180deg);
+  transition: transform .5s ease;
+}
+
+.icons__dm {
+  @apply flex gap-4
+}
+
+.header__dm {
+  @apply justify-between flex items-center text-[#f7f9f9] w-full h-full text-[19px] cursor-pointer font-medium h-[50px]
+}
+
+.header__dm__container {
+  @apply w-full h-full flex items-center px-4
+}
+</style>
---
+++ src/components/icons/IconExpand.vue
@@ -0,0 +1,10 @@
+<template>
+  <svg viewBox="0 0 24 24" aria-hidden="true"
+    class="r-4qtqp9 r-yyyyoo r-1yevf0r r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-meisx5" style="color: rgb(239, 243, 244);">
+    <g>
+      <path
+        d="M12 11.59L3.96 3.54 2.54 4.96 12 14.41l9.46-9.45-1.42-1.42L12 11.59zm0 7l-8.04-8.05-1.42 1.42L12 21.41l9.46-9.45-1.42-1.42L12 18.59z">
+      </path>
+    </g>
+  </svg>
+</template>
